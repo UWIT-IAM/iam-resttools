34e7de461da87fcefbee669f2102515c2dc80e88
Jeff Franklin <jpf@uw.edu>
2018-12-24 07:47:00 -0800

    Fix up package distribution.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# iam-resttools
+# uw-iam-resttools
 
-These are REST clients for several services at UW.
+These are REST clients for IAM services at UW, including IRWS,
+NWS (netid web service), GWS.
 
-Including IRWS, NWS (netid service, GWS, ... 
+GWS v3 returns json payloads, which this client doesn't yet use. More
+appropriate resources can be found at
+
+* [GWS API definition](https://iam-tools.u.washington.edu/apis/gws/)
+* [uw-restclients-gws package](https://pypi.org/project/UW-RestClients-GWS/)
 
 
--- setup.py
+++ setup.py
@@ -6,12 +6,17 @@ VERSION_FILE = os.path.join(BASE_DIR, 'resttools', 'VERSION')
 with open(VERSION_FILE) as f:
     VERSION = f.readlines()[-1].strip()
 
+with open(os.path.join(BASE_DIR, 'README.md')) as f:
+      LONG_DESCRIPTION = f.read()
+
 setup(name='uw-iam-resttools',
       version=VERSION,
       url='https://github.com/UWIT-IAM/iam-resttools',
       author='UW-IT Identity and Access Management',
       author_email='help@uw.edu',
       description='UW-IT REST library for IAM APIs',
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
       license='Apache License, Version 2.0',
       packages=find_packages(),
       include_package_data=True,
