7c61006ee06dd65815393a6ad64d248ecb00ba61
Jeff Franklin <jpf@uw.edu>
2019-08-22 18:49:50 -0700

    Fix pep8.

--- resttools/irws.py
+++ resttools/irws.py
@@ -619,7 +619,7 @@ class IRWS(object):
         subscription.subscription_code = sub_data['subscription_code']
         subscription.subscription_name = sub_data['subscription_name']
         subscription.subscription_data = sub_data.get('subscription_data', None)
-        subscription.status_code = sub_data.get('status_code', None) 
+        subscription.status_code = sub_data.get('status_code', None)
         subscription.status_name = sub_data.get('status_name', None)
         return subscription
 
